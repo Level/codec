9fed84df6d7eff686d7f8d4851cfe8e01e3e0ec7
Julian Gruber <julian@juliangruber.com>
2015-03-18 17:44:32 +0100

    deprecate .encoding

--- test/walk.js
+++ test/walk.js
@@ -4,23 +4,13 @@ var walk = require('../lib/walk');
 test('walk', function(t){
   t.equal(walk(
     [{ keyEncoding: 'utf8' }],
-    ['keyEncoding', 'encoding']
+    'keyEncoding'
   ), 'utf8');
 
   t.equal(walk(
     [{ keyEncoding: 'binary' }, { keyEncoding: 'utf8' }],
-    ['keyEncoding', 'encoding']
+    'keyEncoding'
   ), 'binary');
-
-  t.equal(walk(
-    [{ keyEncoding: 'utf8', encoding: 'buffer' }],
-    ['keyEncoding', 'encoding']
-  ), 'utf8');
-
-  t.equal(walk(
-    [{ encoding: 'utf8' }, { keyEncoding: 'buffer', encoding: 'hex' }],
-    ['keyEncoding', 'encoding']
-  ), 'utf8');
 
   t.end();
 });
