f510a9039ff24b8c31aee0cf59ee7b793e20c6f5
Julian Gruber <julian@juliangruber.com>
2015-03-24 09:51:14 +0100

    new tests for new implementation

---
+++ test/as-buffer.js
@@ -0,0 +1,17 @@
+var test = require('tape');
+var Codec = require('..');
+
+test('key as buffer', function(t){
+  var codec = new Codec({ keyEncoding: 'hex' });
+  t.ok(codec.keyAsBuffer({}));
+  t.notOk(codec.keyAsBuffer({ keyEncoding: 'utf8' }));
+  t.end();
+});
+
+test('value as buffer', function(t){
+  var codec = new Codec({ valueEncoding: 'hex' });
+  t.ok(codec.valueAsBuffer({}));
+  t.notOk(codec.valueAsBuffer({ valueEncoding: 'utf8' }));
+  t.end();
+});
+
--- test/batch.js
+++ test/batch.js
@@ -1,14 +1,15 @@
 var test = require('tape');
-var batch = require('../lib/batch');
+var Codec = require('..');
 
 test('batch', function(t){
+  var codec = new Codec({});
   var ops = [
     { type: 'put', key: 'string', value: 'string', valueEncoding: 'utf8' },
     { type: 'put', key: 'json', value: {} }
   ];
   var opsSerialized = JSON.stringify(ops);
 
-  var encoded = batch(ops, [{ valueEncoding: 'json' }]);
+  var encoded = codec.encodeBatch(ops, { valueEncoding: 'json' });
 
   t.equal(opsSerialized, JSON.stringify(ops), 'ops not changed');
 
