37f103e934436cbd7b2a830810f0d6b3317e7b96
Julian Gruber <julian@juliangruber.com>
2015-03-24 09:50:58 +0100

    settle on new api

--- index.js
+++ index.js
@@ -1,45 +1,69 @@
-var encodeKey = exports.encodeKey = require('./lib/encode-key');
-var encodeValue = exports.encodeValue = require('./lib/encode-value');
-var encodeBatch = exports.encodeBatch = require('./lib/batch');
-var encodings = exports.encodings = require('./lib/encodings');
-var decodeKey = exports.decodeKey = require('./lib/decode-key');
-var decodeValue = exports.decodeValue = require('./lib/decode-value');
-var keyAsBuffer = exports.keyAsBuffer = require('./lib/key-as-buffer');
-var valueAsBuffer = exports.valueAsBuffer = require('./lib/value-as-buffer');
+var encodings = require('./lib/encodings');
 
-exports.Codec = Codec;
+module.exports = Codec;
 
-function Codec(options){
-  this.options = options;
+function Codec(opts){
+  this.opts = opts;
+  this.encodings = encodings;
 }
 
-Codec.prototype.encodeKey = function(key, options){
-  return encodeKey(key, [options, this.options]);
+Codec.prototype._encoding = function(encoding){
+  if (typeof encoding == 'string') encoding = encodings[encoding];
+  if (!encoding) encoding = encodings.id;
+  return encoding;
 };
 
-Codec.prototype.encodeValue = function(value, options){
-  return encodeValue(value, [options, this.options]);
+Codec.prototype._keyEncoding = function(opts, batchOpts){
+  return this._encoding(batchOpts && batchOpts.keyEncoding
+    || opts.keyEncoding
+    || this.opts.keyEncoding);
 };
 
-Codec.prototype.encodeBatch = function(ops, options){
-  return encodeBatch(ops, [options, this.options]);
+Codec.prototype._valueEncoding = function(opts, batchOpts){
+  return this._encoding(batchOpts && batchOpts.valueEncoding
+    || opts.valueEncoding
+    || this.opts.valueEncoding);
 };
 
-Codec.prototype.encodings = encodings;
-
-Codec.prototype.decodeKey = function(key, options){
-  return decodeKey(key, [options, this.options]);
+Codec.prototype.encodeKey = function(key, opts, batchOpts){
+  return this._keyEncoding(opts, batchOpts).encode(key);
 };
 
-Codec.prototype.decodeValue = function(value, options){
-  return decodeValue(value, [options, this.options]);
+Codec.prototype.encodeValue = function(value, opts, batchOpts){
+  return this._valueEncoding(opts, batchOpts).encode(value);
 };
 
-Codec.prototype.keyAsBuffer = function(options){
-  return keyAsBuffer([options, this.options]);
+Codec.prototype.decodeKey = function(key, opts){
+  return this._keyEncoding(opts).decode(key);
 };
 
-Codec.prototype.valueAsBuffer = function(options){
-  return valueAsBuffer([options, this.options]);
+Codec.prototype.decodeValue = function(value, opts){
+  return this._valueEncoding(opts).decode(value);
+};
+
+Codec.prototype.encodeBatch = function(ops, opts){
+  var self = this;
+
+  return ops.map(function(_op){
+    var op = {
+      type: _op.type,
+      key: self.encodeKey(_op.key, opts, _op)
+    };
+    if (self.keyAsBuffer(opts, _op)) op.keyEncoding = 'binary';
+    if (_op.prefix) op.prefix = _op.prefix;
+    if ('value' in _op) {
+      op.value = self.encodeValue(_op.value, opts, _op);
+      if (self.valueAsBuffer(opts, _op)) op.valueEncoding = 'binary';
+    }
+    return op;
+  });
+};
+
+Codec.prototype.keyAsBuffer = function(opts){
+  return this._keyEncoding(opts).buffer;
+};
+
+Codec.prototype.valueAsBuffer = function(opts){
+  return this._valueEncoding(opts).buffer;
 };
 
