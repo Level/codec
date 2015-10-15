54168a06ee6d0b9526e080b97c76d3fba5694ea5
Dominic Tarr <dominic.tarr@gmail.com>
2015-10-15 13:44:51 -0700

    test coverage for legacy "encoding"

--- test/decoder.js
+++ test/decoder.js
@@ -34,3 +34,37 @@ test('createStreamDecoder', function(t){
     t.end();
   });
 });
+
+test('createStreamDecoder - legacy', function(t){
+  var codec = new Codec({ keyEncoding: 'hex' });
+
+  t.test('keys and values', function(t){
+    var decoder = codec.createStreamDecoder({
+      encoding: 'json',
+      keys: true,
+      values: true
+    });
+    t.deepEqual(decoder(new Buffer('hey'), '"you"'), {
+      key: '686579',
+      value: 'you'
+    });
+    t.end();
+  });
+
+  t.test('keys', function(t){
+    var decoder = codec.createStreamDecoder({
+      keys: true
+    });
+    t.equal(decoder(new Buffer('hey')), '686579');
+    t.end();
+  });
+
+  t.test('values', function(t){
+    var decoder = codec.createStreamDecoder({
+      encoding: 'hex',
+      values: true
+    });
+    t.equal(decoder(null, new Buffer('hey')), '686579');
+    t.end();
+  });
+});
