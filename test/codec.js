0a487663d1a79a9cd0a4c9ec2a72c7c67682e149
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-05-12 00:23:55 +0200

    standard: codec already defined

--- test/codec.js
+++ test/codec.js
@@ -4,7 +4,7 @@ var Codec = require('..')
 test('codec', function (t) {
   var codec = new Codec({ keyEncoding: 'hex' })
   t.ok(codec.keyAsBuffer())
-  var codec = new Codec()
+  codec = new Codec()
   t.notOk(codec.keyAsBuffer())
   t.end()
 })
