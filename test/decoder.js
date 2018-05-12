6e7a79fa4368faedaaa86c993faba03e4ed7c382
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-05-12 09:44:23 +0200

    Standard (#29)
    * use and run standard
    
    * standard --fix
    
    * standard: unexpected mix of && and ||
    
    * standard: new Buffer() deprecated
    
    * standard: codec already defined
    
    * remove Makefile :fire:

--- test/decoder.js
+++ test/decoder.js
@@ -1,70 +1,70 @@
-var test = require('tape');
-var Codec = require('..');
+var test = require('tape')
+var Codec = require('..')
 
-test('createStreamDecoder', function(t){
-  var codec = new Codec({ keyEncoding: 'hex' });
+test('createStreamDecoder', function (t) {
+  var codec = new Codec({ keyEncoding: 'hex' })
 
-  t.test('keys and values', function(t){
+  t.test('keys and values', function (t) {
     var decoder = codec.createStreamDecoder({
       valueEncoding: 'json',
       keys: true,
       values: true
-    });
-    t.deepEqual(decoder(new Buffer('hey'), '"you"'), {
+    })
+    t.deepEqual(decoder(Buffer.from('hey'), '"you"'), {
       key: '686579',
       value: 'you'
-    });
-    t.end();
-  });
+    })
+    t.end()
+  })
 
-  t.test('keys', function(t){
+  t.test('keys', function (t) {
     var decoder = codec.createStreamDecoder({
       keys: true
-    });
-    t.equal(decoder(new Buffer('hey')), '686579');
-    t.end();
-  });
+    })
+    t.equal(decoder(Buffer.from('hey')), '686579')
+    t.end()
+  })
 
-  t.test('values', function(t){
+  t.test('values', function (t) {
     var decoder = codec.createStreamDecoder({
       valueEncoding: 'hex',
       values: true
-    });
-    t.equal(decoder(null, new Buffer('hey')), '686579');
-    t.end();
-  });
-});
+    })
+    t.equal(decoder(null, Buffer.from('hey')), '686579')
+    t.end()
+  })
+})
 
-test('createStreamDecoder - legacy', function(t){
-  var codec = new Codec({ keyEncoding: 'hex' });
+test('createStreamDecoder - legacy', function (t) {
+  var codec = new Codec({ keyEncoding: 'hex' })
 
-  t.test('keys and values', function(t){
+  t.test('keys and values', function (t) {
     var decoder = codec.createStreamDecoder({
       encoding: 'json',
       keys: true,
       values: true
-    });
-    t.deepEqual(decoder(new Buffer('hey'), '"you"'), {
+    })
+    t.deepEqual(decoder(Buffer.from('hey'), '"you"'), {
       key: '686579',
       value: 'you'
-    });
-    t.end();
-  });
+    })
+    t.end()
+  })
 
-  t.test('keys', function(t){
+  t.test('keys', function (t) {
     var decoder = codec.createStreamDecoder({
       keys: true
-    });
-    t.equal(decoder(new Buffer('hey')), '686579');
-    t.end();
-  });
+    })
+    t.equal(decoder(Buffer.from('hey')), '686579')
+    t.end()
+  })
 
-  t.test('values', function(t){
+  t.test('values', function (t) {
     var decoder = codec.createStreamDecoder({
       encoding: 'hex',
       values: true
-    });
-    t.equal(decoder(null, new Buffer('hey')), '686579');
-    t.end();
-  });
-});
+    })
+    t.equal(decoder(null, Buffer.from('hey')), '686579')
+    t.end()
+  })
+})
