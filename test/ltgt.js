24b44b8fc59124b3e38bfbc878903b795b3dbda1
Vincent Weevers <mail@vincentweevers.nl>
2021-04-05 00:11:23 +0200

    Remove legacy range options (Level/community#86)

--- test/ltgt.js
+++ test/ltgt.js
@@ -5,21 +5,35 @@ test('encode ltgt', function (t) {
   const codec = new Codec({ keyEncoding: 'hex' })
 
   let ltgt = {
-    start: '686579',
+    gte: '686579',
     lte: '686579'
   }
   let encoded = codec.encodeLtgt(ltgt)
-  t.equal(encoded.start.toString(), 'hey')
+  t.equal(encoded.gte.toString(), 'hey')
   t.equal(encoded.lte.toString(), 'hey')
 
   ltgt = {
-    start: '686579',
+    gte: '686579',
     lte: '686579',
     keyEncoding: 'json'
   }
   encoded = codec.encodeLtgt(ltgt)
-  t.equal(encoded.start, '"686579"')
+  t.equal(encoded.gte, '"686579"')
   t.equal(encoded.lte, '"686579"')
 
   t.end()
+})
+
+test('rejects legacy range options', function (t) {
+  t.plan(2)
+
+  const codec = new Codec()
+
+  for (const k of ['start', 'end']) {
+    try {
+      codec.encodeLtgt({ [k]: 123 })
+    } catch (err) {
+      t.is(err.message, 'Legacy range options ("start" and "end") have been removed')
+    }
+  }
 })
